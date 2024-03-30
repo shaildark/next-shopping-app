cd590500d9bf63b2a47a943679ee11b6e7d29556
Shailendra <shailendra@zealousweb.com>
2024-03-31 00:24:42 +0530

    exception handling added in utils

--- server_functions/utils.js
+++ server_functions/utils.js
@@ -2,14 +2,24 @@ const path = require("path");
 const fs = require("fs");
 
 export async function JsonConvert(data) {
-    return await JSON.parse(JSON.stringify(data));
+    try {
+        return await JSON.parse(JSON.stringify(data));
+    }
+    catch (error) {
+        return [];
+    }
 }
 
 export async function fileUpload(fileObj, uploadDir, newFileName) {
-    const extension = fileObj.name.split(".").pop();
-    const filename = `${newFileName}.${extension}`
-    const destinationPath = path.join(uploadDir, filename);
-    const fileBuffer = await fileObj.arrayBuffer();
-
-    fs.writeFileSync(destinationPath, Buffer.from(fileBuffer));
+    try {
+        const extension = fileObj.name.split(".").pop();
+        const filename = `${newFileName}.${extension}`
+        const destinationPath = path.join(uploadDir, filename);
+        const fileBuffer = await fileObj.arrayBuffer();
+        fs.writeFileSync(destinationPath, Buffer.from(fileBuffer));
+        return true;
+    }
+    catch (error) {
+        return false;
+    }
 }
