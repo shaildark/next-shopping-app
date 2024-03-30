9c4041b059f4e431eb1c55ee3d211a9ea23ac5f1
Shailendra <shailendra@zealousweb.com>
2024-03-31 00:52:40 +0530

    datatable integrated in category index page

--- app/admin/categories/page.js
+++ app/admin/categories/page.js
@@ -1,9 +1,45 @@
+"use client";
+import { allCategories } from "@/server_functions/category";
+import { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
 export default function CategoriesPage() {
+
+    const [categories, setCategories] = useState([]);
+
+    const dataColumns = [
+        {
+            name: "ID",
+            selector: row => row.id,
+        },
+        {
+            name: "Category Name",
+            selector: row => row.vName,
+        },
+    ];
+
+    const addCategoryBtn = <Button><Link href={"/admin/categories/add"}>Add Category</Link></Button>
+
+    useEffect(() => {
+        const getData = async () => {
+            const tempCat = await allCategories();
+            console.log(tempCat)
+            setCategories(tempCat);
+        }
+        getData();
+
+    }, [])
+
     return (
         <>
-            <div>
-                Categories Page
-            </div>
+            <DataTable
+                title="Categories"
+                columns={dataColumns}
+                data={categories}
+                actions={addCategoryBtn}
+            />
         </>
     )
 }
