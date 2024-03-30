20ca173c456cd818bfe89ba94cb9554348f560ea
Shailendra <shailendra@zealousweb.com>
2024-03-31 00:54:06 +0530

    add product page changes

--- app/admin/products/add/page.js
+++ app/admin/products/add/page.js
@@ -19,27 +19,46 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
-import { useState } from "react"
+import { useToast } from "@/components/ui/use-toast"
+import { useState, useEffect } from "react"
+import { allCategories } from "@/server_functions/category"
+import { addProduct } from "@/server_functions/product"
 
 export default function ProductsAddPage() {
+    const { toast } = useToast()
     const [prodName, setProdName] = useState("");
     const [category, setCategory] = useState("");
+    const [prodimage, setProdimage] = useState(null);
     const [errorProduct, setProductError] = useState(false);
-    const submitHandler = (e) => {
+    const [errorCategory, setCategoryError] = useState(false);
+
+    const [categoryOptions, setCategoryOptions] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            let tempData = await allCategories();
+            setCategoryOptions(tempData);
+        }
+
+        getData();
+    }, []);
+
+    const submitHandler = async (e) => {
         e.preventDefault();
-        console.log(category)
         setProductError((prodName === "") ? true : false);
-
-        // Handle Form Submission
-
+        setCategoryError((category === "") ? true : false);
+        const fd = new FormData();
+        fd.append("name", prodName);
+        fd.append("category", category);
+        fd.append("image", prodimage);
+        const result = await addProduct(fd);
+        if (result) {
+            toast({
+                title: "Product",
+                description: "Product added successfully",
+            });
+        }
     }
-
-    const categoryOptions = [
-        { "id": 1, "name": "Home" },
-        { "id": 2, "name": "Computer" },
-        { "id": 3, "name": "Office" },
-        { "id": 4, "name": "Car" }
-    ];
 
     return (
         <>
@@ -60,22 +79,31 @@ export default function ProductsAddPage() {
 
                             <br></br>
 
-                            <Label htmlFor="category">Category</Label>
-                            <Select name="category" onValueChange={(e) => setCategory(e)}>
-                                <SelectTrigger className="w-[180px]">
-                                    <SelectValue placeholder="Select a category" />
-                                </SelectTrigger>
-                                <SelectContent>
+                            <div className="grid w-full max-w-sm items-center gap-1.5">
+                                <Label htmlFor="category">Category</Label>
+                                <Select name="category" onValueChange={(e) => setCategory(e)} className={`${errorCategory ? "outline outline-offset-2 outline-red-500" : ""}`}>
+                                    <SelectTrigger className="w-[180px]">
+                                        <SelectValue placeholder="Select a category" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {
+                                            categoryOptions.map((category) => (
+                                                <SelectItem key={category.id} value={String(category.id)}> {category.vName}</SelectItem>
+                                            ))
+                                        }
+                                    </SelectContent>
                                     {
-                                        categoryOptions.map((category) => (
-                                            <SelectItem key={category.id} value={String(category.id)}> {category.name}</SelectItem>
-                                        ))
+                                        errorCategory && <p className="text-red-500">Category name is required</p>
                                     }
-                                    {/* <SelectItem value="light">Light</SelectItem>
-                                    <SelectItem value="dark">Dark</SelectItem>
-                                    <SelectItem value="system">System</SelectItem> */}
-                                </SelectContent>
-                            </Select>
+                                </Select>
+                            </div>
+
+                            <br></br>
+
+                            <div className="grid w-full max-w-sm items-center gap-1.5">
+                                <Label htmlFor="prodimage">Image</Label>
+                                <Input type="file" name="prodimage" id="prodimage" onChange={(e) => setProdimage(e.target.files[0])} />
+                            </div>
 
                             <br></br>
 
