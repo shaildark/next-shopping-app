ac58dd1c48bf60fbaba92bb5cf2a130f16120c0b
Shailendra <shailendra@zealousweb.com>
2024-03-31 00:25:59 +0530

    code removed

--- models/category.js
+++ models/category.js
@@ -11,13 +11,13 @@ const Category = sequelize.define('Category', {
         type: DataTypes.STRING
     },
     createdAt: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
     },
     updatedAt: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
     },
     deletedAt: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
     }
 }, {
     tableName: 'category',
@@ -25,34 +25,3 @@ const Category = sequelize.define('Category', {
     deletedAt: 'deletedAt',
 });
 module.exports = Category;
-
-
-// const {
-//     Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//     class Category extends Model {
-//         /**
-//          * Helper method for defining associations.
-//          * This method is not a part of Sequelize lifecycle.
-//          * The `models/index` file will call this method automatically.
-//          */
-//         static associate(models) {
-//             // define association here
-//         }
-//     }
-//     Category.init({
-//         id: DataTypes.INTEGER,
-//         vFirstName: DataTypes.STRING,
-//         createdAt: DataTypes.DATE,
-//         updatedAt: DataTypes.DATE,
-//         deletedAt: DataTypes.DATE
-//     }, {
-//         sequelize,
-//         paranoid: true,
-//         deletedAt: 'deletedAt',
-//         modelName: 'category',
-//         freezeTableName: true
-//     });
-//     return Category;
-// };
